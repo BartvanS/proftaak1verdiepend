ff44d747164b988df6c25c8e682f9af0fcefa5e6
Eelke Sneijders <i431739@fhict.nl>
2019-11-27 09:11:46 +0100

    First Version ArduinoBluetooth Visual studio
    Eerste versie van de Visual studio app voor het communiceren met de arduino

---
+++ software/ArduinoBluetoothTest/ArduinoBluetoothTest/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ArduinoBluetoothTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ButtonConnect = new System.Windows.Forms.Button();
+            this.ButtonDisconnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SerialSend = new System.Windows.Forms.TextBox();
+            this.SerialText = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SerialSend);
+            this.groupBox1.Controls.Add(this.ButtonConnect);
+            this.groupBox1.Controls.Add(this.ButtonDisconnect);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(627, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect";
+            // 
+            // ButtonConnect
+            // 
+            this.ButtonConnect.Location = new System.Drawing.Point(397, 38);
+            this.ButtonConnect.Name = "ButtonConnect";
+            this.ButtonConnect.Size = new System.Drawing.Size(92, 23);
+            this.ButtonConnect.TabIndex = 2;
+            this.ButtonConnect.Text = "Stuur";
+            this.ButtonConnect.UseVisualStyleBackColor = true;
+            this.ButtonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // ButtonDisconnect
+            // 
+            this.ButtonDisconnect.Location = new System.Drawing.Point(495, 37);
+            this.ButtonDisconnect.Name = "ButtonDisconnect";
+            this.ButtonDisconnect.Size = new System.Drawing.Size(92, 23);
+            this.ButtonDisconnect.TabIndex = 1;
+            this.ButtonDisconnect.Text = "Uit";
+            this.ButtonDisconnect.UseVisualStyleBackColor = true;
+            this.ButtonDisconnect.Click += new System.EventHandler(this.ButtonDisconnect_Click);
+            // 
+            // SerialSend
+            // 
+            this.SerialSend.Location = new System.Drawing.Point(75, 38);
+            this.SerialSend.Name = "SerialSend";
+            this.SerialSend.Size = new System.Drawing.Size(113, 22);
+            this.SerialSend.TabIndex = 3;
+            // 
+            // SerialText
+            // 
+            this.SerialText.Location = new System.Drawing.Point(13, 136);
+            this.SerialText.Name = "SerialText";
+            this.SerialText.Size = new System.Drawing.Size(627, 157);
+            this.SerialText.TabIndex = 1;
+            this.SerialText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 543);
+            this.Controls.Add(this.SerialText);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ButtonConnect;
+        private System.Windows.Forms.Button ButtonDisconnect;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox SerialSend;
+        private System.Windows.Forms.RichTextBox SerialText;
+    }
+}
+
---
+++ software/ArduinoBluetoothTest/ArduinoBluetoothTest/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoBluetoothTest
+{
+    public partial class Form1 : Form
+    {
+        int count;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.PortName = "COM8";
+            serialPort1.BaudRate = 9600;
+
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        private void DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort spl = (SerialPort)sender;
+                Console.Write("Data" + count + " " + spl.ReadLine() + "/n");
+                count++;
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(SerialSend.Text);
+            SerialSend.Text = " ";
+        }
+
+        private void ButtonDisconnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("f");
+        }
+    }
+}
