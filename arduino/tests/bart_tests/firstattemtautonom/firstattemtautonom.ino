db9ed1e062291b171b7a0d287261677f41732a25
Bart van Sevenbergen <i431686@fhict.nl>
2019-12-04 11:24:55 +0100

    .

--- arduino/tests/bart_tests/firstattemtautonom/firstattemtautonom.ino
+++ arduino/tests/bart_tests/firstattemtautonom/firstattemtautonom.ino
@@ -1,14 +1,17 @@
+//
 int trig = 12;
 int echo = 11;
 long lecture_echo;
 long cm;
-
+//motor shield
 int enA = 10;
 int in1 = 9;
 int in2 = 8;
 int enB = 5;
 int in3 = 4;
 int in4 = 3;
+//test led
+int led = 8;
 
 bool detected = false;
 void setup()
@@ -19,12 +22,14 @@ void setup()
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
-
+  //ultrasone
   pinMode(trig, OUTPUT);
   digitalWrite(trig, LOW);
   pinMode(echo, INPUT);
   Serial.begin(9600);
 
+  //led output
+  pinMode(led, OUTPUT);
 }
 void loop()
 {
@@ -33,28 +38,23 @@ void loop()
   digitalWrite(trig, LOW);
   lecture_echo = pulseIn(echo, HIGH);
   cm = lecture_echo / 58;
-  Serial.print("ultr");
   Serial.println(cm);
-
-  if(cm < 20){
-//    links();
-achteruit();
-//    delay(100);
-//    vooruit();
-  }else{
-      vooruit();
-
+  if (cm < 20)
+  {
+    digitalWrite(led, HIGH);
   }
-  //  if(cm > 20 && detected == false)
-  //  {
-  //    achteruit();
-  //  }else if(cm < 20 && `cted == false){
-  //    detected = true;
-  //    obstacledetected();
-  //  }
-  //}
+  else
+  {
+    digitalWrite(led, LOW);
+  }
 }
-void vooruit() {
+
+
+
+
+
+void vooruit()
+{
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
@@ -63,7 +63,8 @@ void vooruit() {
   analogWrite(enB, 255);
 }
 
-void achteruit() {
+void achteruit()
+{
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -72,7 +73,8 @@ void achteruit() {
   analogWrite(enB, 255);
 }
 
-void rechts() {
+void rechts()
+{
   digitalWrite(in1, LOW); // eerste 2 zijn links 2de rechts
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
@@ -81,7 +83,8 @@ void rechts() {
   analogWrite(enB, 255);
 }
 
-void links() {
+void links()
+{
   digitalWrite(in1, HIGH); // eerste 2 zijn links 2de rechts
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
@@ -90,28 +93,12 @@ void links() {
   analogWrite(enB, 255);
 }
 
-void uit() {
+void uit()
+{
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   analogWrite(enA, 0);
   analogWrite(enB, 0);
-}
-
-
-void obstacledetected()
-{
-  if (detected)
-  {
-    links();
-    delay(200);
-    int linkswaarde = cm;
-    delay(400);
-    rechts();
-    delay(400);
-    int rechtswaard = cm;
-    delay(400);
-    uit();
-  }
 }
