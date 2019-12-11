7342347648a8abd83db527d2f8aa057e46439a74
Bart van Sevenbergen <i431686@fhict.nl>
2019-12-11 09:04:32 +0100

    libraries folder + autonoom refactoring

--- arduino/autonoom/autonoomv1/autonoomv1.ino
+++ arduino/autonoom/autonoomv1/autonoomv1.ino
@@ -17,25 +17,57 @@ void setup()
 
 void loop()
 {
-
+  //        if (currentMillis - previousMillis >= interval) {
+  //          // save the last time you blinked the LED
+  //          previousMillis = currentMillis;
+  //       //ik heb geen idee hoe ik de auto voor een paar seconden met een uninterupted delay kan aansturen.
+  //        }
   unsigned long currentMillis = millis();
   int cm = getDistance();
   Serial.println(cm);
   int count = 0;
   forward(255);
-
-  int goLeft = false;
-  if (cm < minDistance) {
-     goLeft = true;
-//    if (currentMillis - previousMillis >= interval) {
-//      // save the last time you blinked the LED
-//      previousMillis = currentMillis;
-   ////////   //ik heb geen idee hoe ik de auto voor een paar seconden met een uninterupted delay kan aansturen.
-//    }
-    if (goLeft == true) {
-      turnLeft(255);
-    }
-    delay(3000);
-  }
-
+  scanSide("left");
+  //  if(!scanSide("left")){
+  //    turnRight(255);
+  //    delay(1500);
+  //    scanSide("right");
+  //  }
 }
+
+bool scanSide(String side) {
+  int cm = getDistance();
+  int maxTurns = 0;
+  while (cm < minDistance) {
+    maxTurns += 1;
+    cm = getDistance();
+    if (cm > minDistance) {
+      return true;
+    }
+    if (side == "left") {
+      turnLeft(255);
+    } 
+    delay(500);
+  }
+  return false;
+}
+
+
+//bool scanSide(String side) {
+//  int cm = getDistance();
+//  int maxTurns = 0;
+//  while (cm < minDistance && maxTurns < 3) {
+//    maxTurns += 1;
+//    cm = getDistance();
+//    if (cm > minDistance) {
+//      return true;
+//    }
+//    if (side == "left") {
+//      turnLeft(255);
+//    } else {
+//      turnRight(255);
+//    }
+//    delay(500);
+//  }
+//  return false;
+//}
