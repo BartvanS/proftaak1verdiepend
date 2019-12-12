16745ac1021b7361addd28fd5b12b7afd3d58368
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2019-12-12 12:57:47 +0100

    refactor niet getest joebot 1.1

--- arduino/JoeBot1.1/JoeBot1.1.ino
+++ arduino/JoeBot1.1/JoeBot1.1.ino
@@ -1,52 +1,61 @@
 #include <SoftwareSerial.h>   //Software Serial Port
 #include "motor.h"
+#include "ultrasone.h"
 #define RxD 7
 #define TxD 6
-
-#define DEBUG_ENABLED  1
-
+//#define DEBUG_ENABLED  1
+char recvChar;
 SoftwareSerial blueToothSerial(RxD, TxD);
 void setup()
 {
   setupBlueToothConnection();
   Serial.begin(9600);
   initMotor(false);
+  setUltrasone();
   pinMode(RxD, INPUT);
   pinMode(TxD, OUTPUT);
 }
 
 void loop()
 {
-  char recvChar;
+  receiveInput();
+  Serial.println(getDistance());
+}
+
+void receiveInput() {
+  if (Serial.available())
+  { //check if there's any data sent from the local serial terminal, you can add the other applications here
+    recvChar  = Serial.read();
+    blueToothSerial.println(recvChar);
+  }
+
   if (blueToothSerial.available())
   { //check if there's any data sent from the remote bluetooth shield
     recvChar = blueToothSerial.read();
     Serial.print(recvChar);
     if (recvChar == 'w') {
       forward(255);
-      Serial.println("lampje aan.");
+      Serial.println("Forward.");
     } else if (recvChar == 's' ) {
       backwards(255);
+      Serial.println("Backwards");
     } else if (recvChar == 'a' ) {
       turnLeft(255);
+      Serial.println("left");
     } else if (recvChar == 'd' ) {
       turnRight(255);
+      Serial.println("right");
     } else {
       motorOff();
+      Serial.println("STOP");
     }
   }
-  if (Serial.available())
-  { //check if there's any data sent from the local serial terminal, you can add the other applications here
-    recvChar  = Serial.read();
-    blueToothSerial.print(recvChar);
-  }
 }
-
 void setupBlueToothConnection()
 {
   blueToothSerial.begin(38400);                           // Set BluetoothBee BaudRate to default baud rate 38400
   blueToothSerial.print("\r\n+STWMOD=0\r\n");             // set the bluetooth work in slave mode
-  blueToothSerial.print("\r\n+STNA=SeeedBTSlave2\r\n");    // set the bluetooth name as "SeeedBTSlave"
+  blueToothSerial.print("\r\n+STNA=JOE_BOT\r\n");    // set the bluetooth name as "SeeedBTSlave"
   blueToothSerial.print("\r\n+STOAUT=1\r\n");             // Permit Paired device to connect me
   blueToothSerial.print("\r\n+STAUTO=0\r\n");             // Auto-connection should be forbidden here
   delay(2000);                                            // This delay is required.
