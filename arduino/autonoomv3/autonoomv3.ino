f33243deaa496343a8888a8b07e614f4849bcd8d
Jarno Siegers <jarno12227@gmail.com>
2020-01-08 12:58:37 +0100

    Logboek ding + autonoomv3

---
+++ arduino/autonoomv3/autonoomv3.ino
@@ -0,0 +1,55 @@
+#include "motor.h"
+#include "ultrasone.h"
+int cmMiddle;
+int cmRight;
+int cmLeft;
+
+int leftDistance;
+int rightDistance;
+int distance = 20;
+
+void setup() {
+  initMotor(true);
+  setUltrasone();
+  Serial.begin(9600);
+}
+
+
+
+void loop() {
+  cmMiddle = getDistance(1);
+  cmLeft = getDistance(2);
+  cmRight = getDistance(3);
+  Serial.print("Middle: ");
+  Serial.println(cmMiddle);
+  Serial.print("Right: ");
+  Serial.println(cmRight);
+  Serial.print("Left: ");
+  Serial.println(cmLeft);
+  forward(255);
+  if (cmMiddle < distance) {
+    if (cmLeft < cmRight && cmLeft < distance) {
+      turnRight(255);
+    }
+    else if (cmRight < cmLeft && cmRight < distance) {
+      turnLeft(255);
+    } else if (cmRight < distance && cmLeft < distance) {
+      checkSpace(100);
+    } else {
+      checkSpace(100);
+    }
+  } else if (cmRight < distance) {
+    forwardLeft(255);
+  } else if (cmLeft < distance) {
+    forwardRight(255);
+  }
+}
+
+void checkSpace(int motorSpeed) {
+  motorOff();
+  if (cmLeft > cmRight) {
+    turnLeftOnSpot(motorSpeed);
+  } else if (cmRight > cmLeft) {
+    turnRightOnSpot(motorSpeed);
+  }
+}
---
+++ jarnosoftware/logboek/logboek/communicatie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logboek
+{
+    class communicatie
+    {
+        
+        String watInput;
+
+        public int inputInt(String input, String wat)
+        {
+            if (input != "")
+            {
+                string[] getal = input.Split(':');
+
+                if (getal.Length >= 2)
+                {
+                    String gekregen = getal[0];
+                    String[] joe = gekregen.Split('#');
+                    if (joe.Length > 1)
+                    {
+                        watInput = joe[1];
+                    }
+
+                    if (watInput == wat)
+                    {
+                        if (joe.Length > 1)
+                        {
+                            int inputweg = Convert.ToInt32(getal[1]);
+                            return inputweg;
+                        }
+
+                    }
+
+                }
+
+
+            }
+            return 0;
+        }
+    }
+}
