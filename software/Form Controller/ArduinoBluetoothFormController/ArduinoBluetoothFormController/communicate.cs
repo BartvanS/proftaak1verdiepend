0d4381659bd98328f4bc55ea6689b90f9e488463
Cabo Schoenmakers <cabo.schoen@gmail.com>
2020-01-14 08:54:43 +0100

    protocol in controller

---
+++ software/Form Controller/ArduinoBluetoothFormController/ArduinoBluetoothFormController/communicate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoBluetoothFormController
+{
+    
+    class communicate
+    {
+        String whatIsInput;
+        public int inputInt(String input, String wat)
+        {
+            if (input != "")
+            {
+                string[] valueInt = input.Split(':');
+
+                if (valueInt.Length >= 2)
+                {
+                    String gekregen = valueInt[0];
+                    String[] variableName = gekregen.Split('#');
+                    if (variableName.Length > 1)
+                    {
+                        whatIsInput = variableName[1];
+                    }
+
+
+                    if (whatIsInput == wat)
+                    {
+                        if (variableName.Length > 1)
+                        {
+                            return Convert.ToInt32(valueInt[1]);
+                        }
+
+                    }
+
+
+                }
+
+            }
+            return 0;
+
+        }
+    }
+}
---
+++ software/Form Controller/ArduinoBluetoothFormController/ArduinoBluetoothFormController/protocol.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoBluetoothFormController
+{
+    class protocol
+    {
+        String whatInput;
+
+        public int inputInt(String input, String nameSearch)
+        {
+            if (input != "")
+            {
+                string[] getal = input.Split(':');
+                if (getal.Length >= 2)
+                {
+                    String protocolName = getal[0];
+                    String[] numbers = protocolName.Split('#');
+                    if (numbers.Length > 1)
+                    {
+                        whatInput = numbers[1];
+                    }
+                    if (whatInput == nameSearch)
+                    {
+                        if (numbers.Length > 1)
+                        {
+                            return Convert.ToInt32(getal[1]);
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
+
